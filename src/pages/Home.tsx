59005e8947477f31be70ba872c80c210389505a2
Alexeev Vadim <alexeev.vadim@challenge.day>
2023-10-16 12:04:31 +0700

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonAccordion, IonAccordionGroup, IonItem, IonLabel, IonButton, IonPicker } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
@@ -18,6 +18,71 @@ const Home: React.FC = () => {
         </IonHeader>
         <ExploreContainer />
       </IonContent>
+      <IonButton id="open-picker">Open</IonButton>
+      <IonPicker
+        trigger="open-picker"
+        columns={[
+          {
+            name: 'languages',
+            options: [
+              {
+                text: 'Продать Крипту',
+                value: 'selec1',
+              },
+              {
+                text: 'Купить Валюту',
+                value: 'selec2',
+              },
+              {
+                text: 'Обменять Баты',
+                value: 'selec3',
+              },
+              {
+                text: 'Сходить в Кино',
+                value: 'selec3',
+              },
+            ],
+          },
+        ]}
+        buttons={[
+          {
+            text: 'Cancel',
+            role: 'cancel',
+          },
+          {
+            text: 'Confirm',
+            handler: (value) => {
+              console.log(`You selected: ${value.languages.value}`);
+            },
+          },
+        ]}
+      ></IonPicker>
+      <IonAccordionGroup>
+        <IonAccordion value="first">
+          <IonItem slot="header" color="light">
+            <IonLabel>First Accordion</IonLabel>
+          </IonItem>
+          <div className="ion-padding" slot="content">
+            First Content
+          </div>
+        </IonAccordion>
+        <IonAccordion value="second">
+          <IonItem slot="header" color="light">
+            <IonLabel>Second Accordion</IonLabel>
+          </IonItem>
+          <div className="ion-padding" slot="content">
+            Second Content
+          </div>
+        </IonAccordion>
+        <IonAccordion value="third">
+          <IonItem slot="header" color="light">
+            <IonLabel>Third Accordion</IonLabel>
+          </IonItem>
+          <div className="ion-padding" slot="content">
+            Third Content
+          </div>
+        </IonAccordion>
+      </IonAccordionGroup>
     </IonPage>
   );
 };
