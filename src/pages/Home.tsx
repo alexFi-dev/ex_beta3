a9a5dbcbf2fb7c9e8433faf0a1495e3416b4a93a
Alexeev Vadim <alexeev.vadim@challenge.day>
2023-10-16 14:04:08 +0700

    small up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,7 @@ const Home: React.FC = () => {
 
   return (
     <>
+
       <IonHeader>
         <IonToolbar>
           <IonTitle>IONIC UX+UI</IonTitle>
@@ -109,40 +110,7 @@ const Home: React.FC = () => {
         </IonItem>
       </IonList>
 
-      <div className="ion-text-center">    
-      <br /><br /><br /><br /><br />
-        <IonFab>
-          <IonFabButton>
-            <IonIcon icon={add}></IonIcon>
-          </IonFabButton>
-          <IonFabList side="top">
-            <IonFabButton>
-              <IonIcon icon={chevronUp}></IonIcon>
-            </IonFabButton>
-          </IonFabList>
-          <IonFabList side="end">
-            <IonFabButton>
-              <IonIcon icon={chevronForward}></IonIcon>
-            </IonFabButton>
-            <IonFabButton>
-              <IonIcon icon={chevronForward}></IonIcon>
-            </IonFabButton>
-          </IonFabList>
-          <IonFabList side="bottom">
-            <IonFabButton>
-              <IonIcon icon={chevronDown}></IonIcon>
-            </IonFabButton>
-          </IonFabList>
-          <IonFabList side="start">
-            <IonFabButton>
-              <IonIcon icon={chevronBack}></IonIcon>
-            </IonFabButton>
-            <IonFabButton>
-              <IonIcon icon={chevronBack}></IonIcon>
-            </IonFabButton>
-          </IonFabList>
-        </IonFab>
-      </div>
+
     </>
   );
 };
